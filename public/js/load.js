de992ef819696b72e2d8ce46ef859058b9022cf4
Kiricon <kiricon@live.com>
2015-04-08 23:18:57 -0600

    Adding some of the public files I forgot to add at first.

---
+++ public/js/load.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ergo on 12/20/2014.
+ */
+var inputMessageModel = new NewMessageModel;
+var inputMessageView = new NewMessageView ({
+   model : inputMessageModel
+});
+/*
+    var CurrentRelation = new RelationshipModel({ id: '1'});
+
+    CurrentRelation.fetch();
+
+    var CurrentRelationView = new RelationView ({
+        model : CurrentRelation
+    });
+
+*/

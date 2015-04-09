7c5ef3ab4f277280264bb288a5ff5a66a770cd60
Kiricon <kiricon@live.com>
2015-04-09 14:06:40 -0600

    Added a reload button that fetches a new wall. Also included viewport for proper mobile site loading.

--- public/js/Views/WallView.js
+++ public/js/Views/WallView.js
@@ -3,11 +3,17 @@
  */
 var WallView = Backbone.View.extend({
    el: '#wall',
+    events: {
+        "click #newwall": "newwall"
+    },
     initialize: function(){
         this.model.fetch();
         this.model.on('change', this.render, this);
     },
     render: function(){
         this.$el.css('background-image', 'url('+this.model.get('wallpaper')+')');
+    },
+    newwall: function(){
+        this.model.fetch();
     }
 });

ed5d16cc07898e6bf3608e1f9935530dfdad9b00
Kiricon <kiricon@live.com>
2015-04-09 00:51:08 -0600

    Made the controller pass the first wall model in the table to the controller index as json.

--- app/controllers/WallzController.php
+++ app/controllers/WallzController.php
@@ -1,6 +1,20 @@
 <?php
 class WallzController extends \Phalcon\Mvc\Controller {
     public function indexAction() {
-        echo 'nothing here';
+        $request = new Phalcon\Http\Request();
+        $response = new \Phalcon\Http\Response();
+        if($request->isGet()){
+
+            //pull wall table entry/model from table
+            $wall = Wallz::findFirst();
+            //build array from model pulled
+            $data = array('wallpaper' => $wall->wallpaper, 'audio' => $wall->audio);
+
+            //Set the content of the response to our json wall
+            $response->setContent(json_encode($data));
+
+            //Return response
+            return $response;
+        }
     }
 }

5b7c54a50bbd8a63a151ca8845821212f4dbd5c2
Kiricon <kiricon@live.com>
2015-04-11 14:16:16 -0600

    Page now loads embbeded youtube videos. It works by taking a youtube video url, parsing it for the video ID and then placing the ID in the embbeded youtube url.

--- public/js/Views/WallView.js
+++ public/js/Views/WallView.js
@@ -12,11 +12,14 @@ var WallView = Backbone.View.extend({
         this.model.on('sync', this.render, this);
     },
     render: function(){
+        this.$('#video').remove();
         this.$el.css('background-image', 'url('+this.model.get('wallpaper')+')');
-        this.$('#loading').remove();
+        var url = this.model.get('audio').match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+        this.$el.append('<iframe id="video" width="640" height="360" src="https://www.youtube.com/embed/'+url[1]+'?version=3&amp;iv_load_policy=3&amp;rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder=0 allowfullscreen></iframe>');
+        this.$('.loadcontainer').remove();
     },
     newwall: function(){
-        this.$el.append('<div id="loading">LOADING.....</div>');
+        this.$el.append('<div class="loadcontainer"><div id="loading">LOADING.....</div></div>');
         this.model.fetch();
     }
 });

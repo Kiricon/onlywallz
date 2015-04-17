ff9106c4654cfcf1653ed7ab254669e303ceb7fb
Kiricon <kiricon@live.com>
2015-04-17 01:56:32 -0600

    Made some serious UI changes to the submission form along side add some url validation for images and youtube urls.

--- public/js/Views/WallView.js
+++ public/js/Views/WallView.js
@@ -30,7 +30,11 @@ var NewWallView = Backbone.View.extend({
     model: new WallModel,
     events: {
         'click #submit': 'submit',
-        'click #showsubmit': 'display'
+        'click #showsubmit': 'display',
+        'keyup .audio': 'youvalidate',
+        'blur .audio': 'youvalidate',
+        'keyup .wallpaper': 'wallvalidate',
+        'blur .wallpaper': 'wallvalidate'
     },
     submit: function(){
         this.model.set({
@@ -44,6 +48,23 @@ var NewWallView = Backbone.View.extend({
     },
     display: function(){
         this.$('#submitform').toggle();
+    },
+    youvalidate: function(){
+        var match = this.$('.audio').val().match(/^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?(?=.*v=((\w|-){11}))(?:\S+)?$/);
+        if(match){
+            this.$('.audio').css('border', 'solid 2px green');
+        }else{
+            this.$('.audio').css('border', 'solid 2px grey');
+        }
+    },
+    wallvalidate: function(){
+        var image = new Image();
+        image.src = this.$('.wallpaper').val();
+        if(image.width){
+            this.$('.wallpaper').css('border', 'solid 2px green');
+        }else {
+            this.$('.wallpaper').css('border', 'solid 2px grey');
+        }
     }
 
 
